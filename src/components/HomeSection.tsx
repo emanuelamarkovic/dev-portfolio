ee2ca9bfb1760d35664d29034b1aa3a74e6c6d04
emanuela <emanuela.markovic@hotmail.com>
2025-01-22 20:11:02 +0100

    feat: Make section responsive and fix text jumping issue

--- src/components/HomeSection.tsx
+++ src/components/HomeSection.tsx
@@ -15,7 +15,7 @@ const HomeSection = () => {
 
   return (
     <section
-      className="relative bg-first overflow-hidden pt-[3.5rem]"
+      className="relative bg-first overflow-hidden h-screen flex flex-col justify-center items-start"
       id="home"
     >
       <div className="ml-4 relative z-10 p-8 mt-[60vh] md:mt-[15vh]">
@@ -24,13 +24,13 @@ const HomeSection = () => {
           <br />
           I'm Emanuela Markovic.
         </h1>
-        <div className="text-container text-dark text-[3rem] ">
-          I am a{" "}
-          <span id="dynamic-text" className="highlight text-text text-[3rem]">
+        <div className="text-container h-4 mt-4 mb-4 text-first md:text-dark text-[1.5rem] md:text-[2rem] lg:text-[3rem] flex items-center space-x-2">
+          <span id="dynamic-text" className="highlight text-text">
             {currentText}
           </span>
         </div>
-        <div className="flex flex-col md:flex-row md:space-x-4 space-y-4 md:space-y-0 mt-4">
+
+        <div className="flex flex-col md:flex-row md:space-x-4 space-y-4 md:space-y-0 mt-8">
           <Link
             href="/Resume_E_M.pdf"
             target="_blank"
