f1ceb75a5ce7f0302080014b0cfa1511abb7fef0
emanuela <emanuela.markovic@hotmail.com>
2025-01-23 20:47:57 +0100

    Updated colors and improved text styling

--- src/components/ProjectSection.tsx
+++ src/components/ProjectSection.tsx
@@ -5,14 +5,14 @@ const ProjectSection: React.FC = () => {
   return (
     <section
       id="projects"
-      className="min-h-screen bg-gradient-to-br from-background/70 to-foreground/70 pt-40 relative z-10"
+      className="min-h-screen pt-40 relative z-10 bg-gray-100 clip-contact"
     >
       <div className="projects-container max-w-7xl mx-auto px-10 py-16">
         <div className="heading text-center">
           <h3 className="title text-3xl font-semibold mb-4 text-text">
             My Works
           </h3>
-          <div className="separator w-36 h-1 bg-pink-600 mx-auto mb-8"></div>
+          <div className="separator w-36 h-1 bg-text mx-auto mb-8"></div>
           <p className="subtitle text-lg mx-auto text-first text-justify w-3/4 mb-6">
             Here's a list of <u>most</u> of the projects I've been working on
             lately. All of these projects were created during my DCI training at
@@ -20,7 +20,7 @@ const ProjectSection: React.FC = () => {
               href="https://digitalcareerinstitute.org/"
               target="_blank"
               rel="noopener noreferrer"
-              className="text-pink-600 hover:underline"
+              className="text-text hover:underline"
             >
               DCI
             </a>
@@ -68,7 +68,7 @@ const ProjectSection: React.FC = () => {
           <Project
             title="Daily-Tasks-frontend"
             img="/images/dailyTasks.png"
-            tech="js react-native css"
+            tech="js react css"
             link="https://emanuelamarkovic.github.io/"
             repo="https://github.com/emanuelamarkovic/daily-tasks-frontend"
           >
@@ -83,7 +83,7 @@ const ProjectSection: React.FC = () => {
           <Project
             title="Daily-Tasks-backend"
             img="/images/dailyTasksBackend.png"
-            tech="node mongodb express"
+            tech="node express mongodb"
             link="https://emanuelamarkovic.github.io/"
             repo="https://github.com/emanuelamarkovic/daily-tasks-backend"
           >
